5d5b96f32db0d79a32fe98a639e56b2c9a528dc4
Apex <gonz.anthony6@gmail.com>
2022-07-14 14:47:18 -0700

    Adding login/sign up routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,12 +7,28 @@ router.post("/", async (req, res) => {
   const { fullName, username, password, profile_picture } = req.body;
   const createdUser = bcrypt.hash(password, 10).then((hash) => {
     Users.create({
-      fullName: fullName,
-      username: username,
+      fullName: fullName.payload,
+      username: username.payload,
       password: hash,
     //   profile_picture: profile_picture,
     });
   });
   res.json(username);
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await Users.findOne({ where: { username: username.payload } });
+  if (!user) {
+    res.json({ error: "user does not exist" });
+  } else {
+    bcrypt.compare(password, user.password).then((match) => {
+      if (!match) {
+        res.json({ error: "Wrong username or password" });
+      } else {
+        res.json(user);
+      }
+    });
+  }
 });
 module.exports = router;
