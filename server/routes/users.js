7452508745fbc9c50906f24e4efabed2fe7debc5
Apex <gonz.anthony6@gmail.com>
2022-07-15 12:59:35 -0700

    Valid/Not Valid password bug fix

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,34 +2,54 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const { Users } = require("../models");
+const { Op } = require("sequelize");
+const e = require("express");
 
 router.post("/", async (req, res) => {
   const { fullName, username, password, profile_picture } = req.body;
-  const createdUser = bcrypt.hash(password, 10).then((hash) => {
+  const createdUser = bcrypt.hash(password.payload, 10).then((hash) => {
     Users.create({
       fullName: fullName.payload,
       username: username.payload,
+      //password hashing payload as well as input password
       password: hash,
-    //   profile_picture: profile_picture,
+      profile_picture: profile_picture,
     });
   });
-  res.json(username);
+  res.json(password);
 });
 
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const user = await Users.findOne({ where: { username: username.payload } });
+  const user = await Users.findOne({  where: { username: username.payload } } );
   if (!user) {
-    res.json({ error: "user does not exist" });
-  } else {
-    bcrypt.compare(password, user.password).then((match) => {
-      if (!match) {
-        res.json({ error: "Wrong username or password" });
-      } else {
-        res.json(user);
-      }
-    });
-  }
+    res.json({ error: "User does not exist." });
+  } 
+  if(user){
+    const validPass = await bcrypt.compare(password.payload, user.password);
+    if(validPass){
+      console.log("valid")
+    }else {
+      console.log("not valid");
+      console.log(password);
+      console.log(user.password)
+    }
+    }
+  // if (user){
+  //   bcrypt.hash(password.payload, 10).then(hash =>{
+      // bcrypt.compare(hash, user.password, function(err, result) {
+        // if (result) {
+        //   console.log("It matches!")
+        // }
+        // else {
+        //   console.log("Invalid password!");
+        //   console.log(user.password);
+        //   console.log(hash);
+        //   //Input password not matching
+        // }
+  //       });
+  //   })
+  // }
 });
 router.get("/:username", async(req, res)=>{
   const user = req.params.username;
